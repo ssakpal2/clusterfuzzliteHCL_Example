9c8874321be4a48d8a5171a5b23e4c5bc83497ff
ssakpal2 <104564045+ssakpal2@users.noreply.github.com>
2024-06-14 13:59:48 +0200

    Update calculator.cpp

--- calculator.h
+++ calculator.h
@@ -1,19 +1,71 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
+///
+/// @file   test.cpp
+/// @brief  test program for calculator.hpp
+///
+/// Copyright (C) 2013 Kim Walisch, <kim.walisch@gmail.com>
+///
+/// This file is distributed under the New BSD License. See the
+/// LICENSE file in the top level directory.
+///
 
-class Calculator {
-private:
-    int precision;  // Precision for floating-point results
+#include "calculator.hpp"
+#include <iostream>
+#include <iomanip>
+#include <string>
 
-public:
-    Calculator();  // Constructor
+#define STR1(s) #s
+#define TOSTRING(s) STR1(s)
 
-    void configure(int precision);  // Configure calculator precision
+/// Test expressions
+#define EXPR1 45345 + 0 + 0xdf234 - 1000 % 7
+#define EXPR2 (0 + 0xdf234 - 1000) * 3 / 2 % 999
+#define EXPR3 1 << 16
+#define EXPR4 (0 + ~(0xdf234 & 1000) * 3) / -2
+#define EXPR5 ((1 << 16) + (1 << 16)) >> 0X5
+#define EXPR6 1+(((2+(3+(4+(5+6)* -7)/8))&127)<<1) *-3
+#define EXPR7 100000000 + (1 << 16) + (1 << 16)
+#define EXPR8 1-~1
+#define EXPR9 1- ~1*0xfFa/( ((((8+(6|(4 *(2*(1)*3)*5)|7)+9)))))
+#define EXPRa ((12|13)<<8)>>((1|127) %10&(31+7))
+#define EXPRb ((((((((((5))))))  ))))- ((((((((( 6)))))))))
 
-    double add(double a, double b);  // Addition operation
-    double subtract(double a, double b);  // Subtraction operation
-    double multiply(double a, double b);  // Multiplication operation
-    double divide(double a, double b);  // Division operation
-};
+int failed = 0;
 
-#endif // CALCULATOR_H
+void compare(int result, const std::string& str)
+{
+  int r = calculator::eval(str);
+  std::cout << (r == result ? "Correct: " : "Error: ");
+  std::cout << std::setw(50) << str << " = " << std::setw(10) << r;
+  if (r != result)
+  {
+    std::cout << " != " << result;
+    failed++;
+  }
+  std::cout << std::endl;
+}
+
+int main()
+{
+  std::cout.setf(std::ios::left);
+
+  compare(EXPR1, TOSTRING(EXPR1));
+  compare(EXPR2, TOSTRING(EXPR2));
+  compare(EXPR3, TOSTRING(EXPR3));
+  compare(EXPR4, TOSTRING(EXPR4));
+  compare(EXPR5, TOSTRING(EXPR5));
+  compare(EXPR6, TOSTRING(EXPR6));
+  compare(EXPR7, TOSTRING(EXPR7));
+  compare(EXPR8, TOSTRING(EXPR8));
+  compare(EXPR9, TOSTRING(EXPR9));
+  compare(EXPRa, TOSTRING(EXPRa));
+  compare(EXPRb, TOSTRING(EXPRb));
+
+  if (failed != 0)
+  {
+    std::cerr << failed << " test(s) failed!" << std::endl;
+    return 1;
+  }
+
+  std::cout << "All tests passed successfully!" << std::endl;
+  return 0;
+}
