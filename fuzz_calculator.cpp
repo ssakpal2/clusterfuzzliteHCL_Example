12a21904eeedc640206d509e3c99c3b2c6121016
ssakpal2 <104564045+ssakpal2@users.noreply.github.com>
2024-06-15 16:57:43 +0200

    Update fuzz_calculator.cpp

--- fuzz_calculator.cpp
+++ fuzz_calculator.cpp
@@ -5,7 +5,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     // Convert input data into a string
     std::string Input(reinterpret_cast<const char*>(Data), Size);
 
-    // Call calculator::eval with the fuzzed input
+    // Call calculator::eval with the fuzzed input 
     try {
         calculator::eval<int>(Input);
     } catch (const calculator::error&) {
